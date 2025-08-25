c1120d4f6f621235ec6b24c32e31b9c4b8b9ccab
Shikha Singh <88542318+singhcodes26@users.noreply.github.com>
2025-08-25 12:45:00 +0530

    Update selection_sort1.cpp

--- Lecture_19_Selection_Sort/selection_sort1.cpp
+++ Lecture_19_Selection_Sort/selection_sort1.cpp
@@ -1,50 +1,33 @@
-// C++ program to implement Selection Sort
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-void selectionSort(vector<int> &arr) {
-    int n = arr.size();
-
-    for (int i = 0; i < n - 1; ++i) {
-
-        // Assume the current position holds
-        // the minimum element
-        int min_idx = i;
-
-        // Iterate through the unsorted portion
-        // to find the actual minimum
-        for (int j = i + 1; j < n; ++j) {
-            if (arr[j] < arr[min_idx]) {
-
-                // Update min_idx if a smaller
-                // element is found
-                min_idx = j; 
-            }
-        }
-
-        // Move minimum element to its
-        // correct position
-        swap(arr[i], arr[min_idx]);
-    }
-}
-
-void printArray(vector<int> &arr) {
-    for (int &val : arr) {
-        cout << val << " ";
-    }
-    cout << endl;
-}
-
 int main() {
-    vector<int> arr = {64, 25, 12, 22, 11};
 
-    cout << "Original array: ";
-    printArray(arr); 
+int arr[5]={7,8,2,3,5};
 
-    selectionSort(arr);
+for(int i=0;i<4;i++){ I
 
-    cout << "Sorted array: ";
-    printArray(arr);
+int index = i;
 
-    return 0;
+for(int j = i+1;j<5;j++) {
+
+if(arr[index]>arr[j]){
+
+swap(arr[index], arr[_
+
+}
+
+{
+
+}
+
+for(int i =0;i<5;i++){
+
+cout<<arr[i]<<" ";
+
+}
+
+return 0;
+
 }
