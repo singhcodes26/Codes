12774d4ea358c5360a9984bbb07973327f9dd7b8
Shikha Singh <88542318+singhcodes26@users.noreply.github.com>
2025-08-26 14:36:43 +0530

    Update selection_sort4.cpp

--- Lecture_19_Selection_Sort/selection_sort4.cpp
+++ Lecture_19_Selection_Sort/selection_sort4.cpp
@@ -1,1 +1,30 @@
 //sort the array of char in ascending order
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+char arr[5]={'c','a','m','g','i'};
+
+for(int i = 0; i<4;i++){
+
+int index = i;
+
+for(int j=i+1;j<5;j++){ if(arr[index]>arr[j]){ swap(arr[index],arr[j]);
+
+}
+
+}
+
+}
+
+for(int i=0;i<5;i++){
+
+cout<<arr[i]<<" ";
+
+}
+
+return 0;
+
+}
