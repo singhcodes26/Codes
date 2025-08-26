49f135165e9408d488459af03e53ce3919ff2715
Shikha Singh <88542318+singhcodes26@users.noreply.github.com>
2025-08-26 14:38:01 +0530

    Update selection_sort2.cpp

--- Lecture_19_Selection_Sort/selection_sort2.cpp
+++ Lecture_19_Selection_Sort/selection_sort2.cpp
@@ -1,1 +1,32 @@
-//decreasing order
+//decreasing orde
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int arr[5]={7,1,4,8,2};
+
+for(int i = 0;i<4;i++){
+
+int index = i;
+
+for(int j=i+1;j<5;j++){ if(arr[index]<arr[j]){
+
+swap(arr[index],arr[j]);
+
+}
+
+}
+
+}
+
+for(int i=0;i<5;i++){
+
+cout<<arr[i]<<" ";
+
+}
+
+return 0;
+
+}
