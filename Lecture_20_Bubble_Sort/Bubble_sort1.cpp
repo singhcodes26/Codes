c626ee4de695de4b6f75ecc9ec6ecb6381bfc542
Shikha Singh <88542318+singhcodes26@users.noreply.github.com>
2025-08-26 13:28:05 +0530

    Update Bubble_sort1.cpp

--- Lecture_20_Bubble_Sort/Bubble_sort1.cpp
+++ Lecture_20_Bubble_Sort/Bubble_sort1.cpp
@@ -1,1 +1,33 @@
+// bubble sort in increasing order
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int arr [5] = \{7, 4, 1, 6, 9\}
+
+for(int i = 5 ; i >= 0 ;i--) {
+
+for(int j = 0 ; j < i - 1 ;j++){
+
+if(arr[j]>arr[j+1]){
+
+swap(arr[j],arr[j+1]);
+
+}
+
+}
+
+}
+
+for(int i = 0 ; i < 5 ;i++){
+
+cout<<arr[i]<<" ";
+
+}
+
+return 0;
+
+}
