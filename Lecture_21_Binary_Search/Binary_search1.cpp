54db48c455ea6429449df3403a5c3a4cd92c0add
Shikha Singh <88542318+singhcodes26@users.noreply.github.com>
2025-08-28 13:25:34 +0530

    Update Binary_search1.cpp

--- Lecture_21_Binary_Search/Binary_search1.cpp
+++ Lecture_21_Binary_Search/Binary_search1.cpp
@@ -1,1 +1,54 @@
+// binary search in ascending array
 
+#include <iostream>
+
+using namespace std;
+
+int BinarySearch(int arr[], int n,int key){
+
+int start =0, end n-1,mid;
+
+while(start<=end) {
+
+mid start (end-start)/2;
+
+if(arr[mid]==key){
+
+}
+
+cout<<"found"<<endl;
+
+return mid;
+
+break;
+
+else if(arr[mid]<key)
+
+start = mid+1;
+
+}
+
+else
+
+end = mid-1;
+
+}
+
+return -1;
+
+}
+
+int main() {
+
+int n = 12;
+
+//cin>>n;
+
+int key = 21;
+
+int arr[n]={2,4,5,7,9,11,12,16,18,20,24,27};
+
+cout<<BinarySearch(arr,n,key)<<endl;
+
+return 0;
+}
