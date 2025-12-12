05f0dc5d1c4ba5295f27b32e48d9b8a487531a86
Shikha Singh <88542318+singhcodes26@users.noreply.github.com>
2025-12-12 21:39:32 +0530

    Update Column_Sum.cpp

--- Lecture_31_2DArray/Column_Sum.cpp
+++ Lecture_31_2DArray/Column_Sum.cpp
@@ -1,1 +1,30 @@
+//sum of each column in 2D array
+#include <iostream>
 
+using namespace std;
+
+int main() {
+
+int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+
+int row = 3;
+
+int col = 3;
+
+int sum = 0;
+
+M365
+
+sum+= arr[i][j];
+
+for(int i=0;i<row;i++){ sum = 0; for(int j=0;j<col;j++){ } }
+
+cout<<"Column "<<i<<" sum is "
+
+<<sum;
+
+cout<<endl;
+
+return 0;
+
+}
